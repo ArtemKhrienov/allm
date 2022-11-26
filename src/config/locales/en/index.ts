570754991686e63466d3d9451cae1fcc37d01aca
Artem Khrienov <artemkhrenov@gmail.com>
2022-11-25 16:16:35 -0800

    Add en locale to the application

--- src/config/locales/en/index.ts
+++ src/config/locales/en/index.ts
@@ -1,5 +1,7 @@
-import DASHBOARD_EN from './dashboard.json';
+import GLOBAL_EN from './global.locale.json';
+import DASHBOARD_EN from './dashboard.locale.json';
 
 export default {
+  global: GLOBAL_EN,
   dashboard: DASHBOARD_EN,
 };
--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import config from '../../config';
+import { useTranslation } from 'react-i18next';
 
 import SignInForm from '../../components/forms/sign-in-form/sign-in-form.component';
 
@@ -10,6 +9,8 @@ import logoImg from '../../assets/images/logo.png';
 import { GradientBackground } from './authentication.styles';
 
 const Authentication = () => {
+  const { t } = useTranslation('global');
+
   return (
     <>
       <GradientBackground />
@@ -19,22 +20,22 @@ const Authentication = () => {
             <img
               className="mx-auto max-w-full w-11 mb-2 md:w-14"
               src={logoImg}
-              alt={config.env.REACT_APP_TITLE}
+              alt={t('app.name') as string}
             />
 
             <div className="text-center mb-7">
               <h1 className="text-gray-700 text-xl font-bold mb-2 md:text-2xl">
-                Welcome to {config.env.REACT_APP_TITLE}
+                Welcome to {t('app.name')}
               </h1>
               <div className="text-gray-400 font-medium text-xs md:text-sm">
-                {config.env.REACT_APP_DESCRIPTION}
+                {t('app.description.full')}
               </div>
             </div>
 
             <img
               className="mx-auto max-w-full w-36 mb-7 md:w-64"
               src={welcomeImg}
-              alt={config.env.REACT_APP_DESCRIPTION}
+              alt={t('app.description.full') as string}
             />
 
             <SignInForm />
